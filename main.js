33e9fc8270fe93fd3b25036c1a54c4d6d2a0d6a7
Kamil Milewski <milewski.kamil93@gmail.com>
2020-05-12 11:21:15 +0200

    Third formatted documents

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const firstName = "Kamil";
 const age = 27;
 
-console.log(`Siema, nazywam się ${firstName} i mam ${age} lat.`);
+console.log(`Witaj drogi gościu, nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector('.main-heading--js');
+
+heading.innerHTML = `Witaj drogi gościu, nazywam się ${firstName} i mam ${age} lat.`
