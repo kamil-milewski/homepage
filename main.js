3fa38454ee3f26011157e6823d8ff42d0ba1d351
Kamil Milewski <milewski.kamil93@gmail.com>
2020-06-09 13:57:07 +0200

    life in a box

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const age = 27;
 
 console.log(`Witaj drogi gościu, nazywam się ${firstName} i mam ${age} lat.`);
 
-const heading = document.querySelector('.main-heading--js');
 
-heading.innerHTML = `Witaj drogi gościu, nazywam się ${firstName} i mam ${age} lat.`
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+
+emptyParagraph.innerHTML = `Nawet uzupełniłem treść javascriptem`;
