a7a4b161157a33a82bf87c63470ded266535adde
Kamil Milewski <milewski.kamil93@gmail.com>
2023-07-31 12:13:17 +0200

    greeting function

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ console.log(age);
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
 
+const greet = (name, age) => {
+    const result = `Siema ${name}! Masz ${age} lat!`;
+    return result;
+}
+
+console.log(greet('Kamil', 30))
+
 const contentJs = document.querySelector('.article__content--js');
 
 contentJs.innerHTML = 'To ja, Twoje wypełnienie JS 👨‍💻'
